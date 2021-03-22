dae2f99c8e081a2abe547369096b3274c0208249
TalAkshlomo <talakshlomo@yahoo.com>
2021-03-22 21:49:13 +0000

    I tried fixing some of the code but I will try finishing the rest

--- main.fs
+++ main.fs
@@ -5,10 +5,19 @@ type Tree =
     | Leaf of int
 
 let rec prod (t:Tree) :int =
-    0
+    let test = Node 23
+    match test with
+    | Node t
+    | Leaf l when t > 23 -> t
+    | _ -> 1
 
 let rec map (f:int->int) (t:Tree) :Tree =
     t
 
 let rec foldStr (nf:string -> string -> string) (lf:int->string) (t:Tree) :string =
     ""
+
+// let result (r:Error) :int =
+//    match r with
+//    OK -> 0 
+//    Err n -> n
